2f3c1b2094c336093657c28b3bb22bba8e85b7ef
Matiarriete <47977071+Matiarriete@users.noreply.github.com>
2022-09-15 17:10:04 +0200

    Agrego Leng y habilidades

---
+++ scripts/habilidades.js
@@ -0,0 +1,28 @@
+var array = [['Java',40], ['HTML', 75], ['C++', 15], ['MySQL', 50],
+['JavaScript', 40], ['Ux Design', 75], ['Google Scripts', 15], ['NodeJS', 15]
+['CSS', 75]];
+
+ array.forEach(element => {
+    const abilities = document.getElementById('abilities');
+
+    const contAbility = document.createElement('div');
+    const p = document.createElement('p');
+    const progress = document.createElement('div');
+    const progressbar = document.createElement('div');
+
+    contAbility.classList.add('contAbility');
+    progress.classList.add('progress');
+    progressbar.classList.add('progress-bar');
+
+    p.textContent = element[0];
+    progressbar.ariaLabel = "Basic example"
+    progressbar.ariaValueMax = "100"
+    progressbar.ariaValueMin = "0"
+    progressbar.ariaValueNow = "75"
+    progressbar.style = "width: " + element[1].toString() + "%"
+
+    progress.appendChild(progressbar);
+    contAbility.appendChild(p);
+    contAbility.appendChild(progress);
+    abilities.appendChild(contAbility);
+ });
